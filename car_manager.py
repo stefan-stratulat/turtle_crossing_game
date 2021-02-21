cb7878f534cf408b056754b115c181c201197f86
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 18:10:47 +0200

    reworked generate_car and move_car methods

--- car_manager.py
+++ car_manager.py
@@ -6,15 +6,25 @@ STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager(Turtle):
+class CarManager():
 
     def __init__(self):
-        super().__init__()
-        self.car()
+        self.cars = []
 
-    def car(self):
-        self.color(random.choice(COLORS))
-        self.penup()
-        self.shape('square')
-        self.shapesize(stretch_wid=1,stretch_len=2)
-        self.setheading(180)
+    def generate_car(self):
+        new_car = Turtle()
+        new_car.shape('square')
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_wid=1,stretch_len=2)
+        new_car.setheading(180)
+        new_car.hideturtle()
+        new_car.x_cor = 280
+        new_car.y_cor = random.randint(-260,280)
+        new_car.goto(new_car.x_cor,new_car.y_cor)
+        self.cars.append(new_car)
+
+    def move_car(self):
+        for car in self.cars[::3]:
+            car.showturtle()
+            car.forward(MOVE_INCREMENT)
