414cfb704cd386989d231412dcda5d25a1b5c5bc
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 15:39:03 +0200

    car method

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.car()
+
+    def car(self):
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_wid=1,stretch_len=2)
+        self.setheading(180)
