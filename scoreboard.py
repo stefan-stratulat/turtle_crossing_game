1f91e7d140877fa35ed7bb4045363d487f642186
sstefan99 <66162946+sstefan99@users.noreply.github.com>
2021-02-21 18:27:18 +0200

    created scoreboard and increase level

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,29 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
+class Scoreboard(Turtle):
 
-class Scoreboard:
-    pass
+    def __init__(self):
+
+        super().__init__()
+        self.color('black')
+        self.hideturtle()
+        self.penup()
+        self.goto(-260,260)
+        self.level = 1
+        self.write(f"Level: {self.level} ",font=FONT)
+
+    def increase_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level} ",font=FONT)
+
+    def game_over(self)
+        self.color('black')
+        self.hideturtle()
+        self.penup()
+        self.write('GAME OVER.', font=FONT)
+
+
+
